f4023d2d96ced5fa9fd062fe3752c511cbd001df
Aloysio <aloysiogl@gmail.com>
2020-05-29 21:42:54 -0300

    Created ball oscilator class

--- src/objects/water/WaterOscillator.cpp
+++ src/objects/water/WaterOscillator.cpp
@@ -3,3 +3,16 @@
 //
 
 #include "WaterOscillator.h"
+
+WaterOscillator::WaterOscillator(vcl::vec2 position, float timestep, float charge = 1.0f, float amplitude = 1.0f,
+                                 float initial_phase = 0.0f) : postion(position), timestep(timestep),
+        charge(charge), amplitude(amplitude), phase(initial_phase) {}
+
+vcl::vec3 WaterOscillator::step() {amplitude*sin(phase);
+    phase += timestep;
+    return {postion.x, postion.y, amplitude*sin(phase)};
+}
+
+vcl::vec3 WaterOscillator::getPosition() {
+    return {postion.x, postion.y, amplitude*sin(phase)};
+}
--- src/objects/water/WaterOscillator.h
+++ src/objects/water/WaterOscillator.h
@@ -5,9 +5,22 @@
 #ifndef PGM_WATEROSCILLATOR_H
 #define PGM_WATEROSCILLATOR_H
 
+#include "vcl.hpp"
 
 class WaterOscillator {
+public:
+    WaterOscillator(vcl::vec2 position, float timestep, float charge, float amplitude,
+                    float initial_phase);
 
+    vcl::vec3 step();
+
+    vcl::vec3 getPosition();
+private:
+    vcl::vec2 postion;
+    float timestep;
+    float charge;
+    float amplitude;
+    float phase;
 };
 
 
